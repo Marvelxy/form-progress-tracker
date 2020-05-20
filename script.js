2bb12a8caac9c7ab7a5664fc0ff7d5ad9315803f
Marvelous Akpotu <still4marvelous@gmail.com>
2020-05-20 22:16:41 +0100

    Check name checkbox if name field is filled.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 	let name = document.querySelector('#name');
 	let progress = document.querySelector('#progress-color');
 
+	let nameCheckBox = document.querySelector('#name-checkbox');
 
 	name.onfocus = inputFocus;
 	name.onblur = inputBlur;
@@ -15,7 +16,10 @@
 	  //name.value = 'Focus has been lost';
 	  if(name.value){
 	  	progress.classList.add('p10');
+	  	nameCheckBox.setAttribute('checked', 'checked');
 	  }
+
+	  console.log()
 	}
 
 })();
