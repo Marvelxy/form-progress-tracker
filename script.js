794765c8fe8cb8694021c8620af7b7706bb74649
Marvelous Akpotu <still4marvelous@gmail.com>
2020-05-20 22:06:37 +0100

    Working on the progress bar and checkboxes.

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 (function() {
-	
+	//progress-color
+	let name = document.querySelector('#name');
+	let progress = document.querySelector('#progress-color');
+
+
+	name.onfocus = inputFocus;
+	name.onblur = inputBlur;
+
+	function inputFocus() {
+	  //name.value = 'Focus is here';
+	}
+
+	function inputBlur() {
+	  //name.value = 'Focus has been lost';
+	  if(name.value){
+	  	progress.classList.add('p10');
+	  }
+	}
 
 })();
