42b26233db19f29ffb7136150f7ddac054f1f14e
Marvelous Akpotu <still4marvelous@gmail.com>
2020-05-21 23:17:37 +0100

    Add email progress and checked checkbox.

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 (function() {
 	//progress-color
-	let name = document.querySelector('#name');
 	let progress = document.querySelector('#progress-color');
 
+	//name.onfocus = inputFocus;
+	//name.onblur = inputBlur;
+
+	let name = document.querySelector('#name');
 	let nameCheckBox = document.querySelector('#name-checkbox');
 
-	name.onfocus = inputFocus;
-	name.onblur = inputBlur;
+	name.addEventListener('blur', function(){
+		if(name.value){
+		  	progress.classList.add('p10');
+		  	nameCheckBox.checked = true;
+		}
+		else{
+		  	progress.classList.remove('p10');
+		  	nameCheckBox.checked = false;
+		}
+	});
 
-	function inputFocus() {
+	let email = document.querySelector('#email');
+	let emailCheckBox = document.querySelector('#email-checkbox');
+
+	email.addEventListener('blur', function(){
+		if(email.value){
+		  	progress.classList.add('p20');
+		  	emailCheckBox.checked = true;
+		}
+		else{
+		  	progress.classList.remove('p20');
+		  	emailCheckBox.checked = false;
+		}
+	});
+
+	/*function inputFocus() {
 	  //name.value = 'Focus is here';
-	}
+	}*/
 
 	function inputBlur() {
 	  //name.value = 'Focus has been lost';
